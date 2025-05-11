2437cb12715b2145fbf670a59792b4a3742c8891
BlueCool12 <pmini12@icloud.com>
2025-05-11 09:10:00 +0900

    20250511_글 리스트 쿼리 변경

--- src/main/java/com/pyomin/cool/repository/PostRepository.java
+++ src/main/java/com/pyomin/cool/repository/PostRepository.java
@@ -1,12 +1,17 @@
 package com.pyomin.cool.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.pyomin.cool.domain.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query("SELECT p FROM Post p WHERE p.isPublic = true AND p.isDeleted = false ORDER BY p.createdAt DESC")
+    List<Post> findVisiblePosts();
 
     Optional<Post> findBySlug(String slug);
 }
