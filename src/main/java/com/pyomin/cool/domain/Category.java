38ed2b46f6585b72fda067001a31c6f0d638a2e8
BlueCool12 <pmini12@icloud.com>
2025-06-11 08:30:22 +0900

    20250611_카테고리 엔티티 수정

--- src/main/java/com/pyomin/cool/domain/Category.java
+++ src/main/java/com/pyomin/cool/domain/Category.java
@@ -8,7 +8,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 
@@ -23,6 +26,13 @@ public class Category {
 
     @Column(nullable = false, unique = true)
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> children = new ArrayList<>();
 
     @ManyToMany(mappedBy = "categories")
     private List<Post> posts = new ArrayList<>();
