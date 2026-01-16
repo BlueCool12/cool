d36f9c12d441ec05ddb90f14dfbaba6b9138eec1
BlueCool12 <pmini12@icloud.com>
2026-01-17 05:11:55 +0900

    feat(post): PageView 테이블 slug 컬럼 추가 및 인덱스 적용

--- src/main/java/com/pyomin/cool/repository/PostRepository.java
+++ src/main/java/com/pyomin/cool/repository/PostRepository.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.pyomin.cool.domain.Post;
 import com.pyomin.cool.domain.PostStatus;
@@ -80,6 +81,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
             """)
     List<SitemapDto<String>> findAllForSitemap(PostStatus status);
 
+    @Transactional
     @Modifying(clearAutomatically = true)
     @Query("UPDATE Post p SET p.viewCount = p.viewCount + 1 WHERE p.slug = :slug")
     void incrementViewCountBySlug(@Param("slug") String slug);
