5f5737ca66f405bdd80af9f8083a652169845ab5
BlueCool12 <pmini12@icloud.com>
2026-02-01 10:30:54 +0900

    feat(comment): 리스트 응답에 adminId 필드 추가
    - 프론트엔드에서 관리자 렌더링을 위한 데이터 제공

--- src/main/java/com/pyomin/cool/dto/CommentListDto.java
+++ src/main/java/com/pyomin/cool/dto/CommentListDto.java
@@ -3,7 +3,6 @@ package com.pyomin.cool.dto;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.pyomin.cool.domain.Comment;
@@ -16,6 +15,7 @@ import lombok.Getter;
 public class CommentListDto {
 
     private final Long id;
+    private final Long adminId;
     private final String nickname;
     private final String content;
 
@@ -25,21 +25,10 @@ public class CommentListDto {
     private final OffsetDateTime createdAt;
     private final List<CommentListDto> children;
 
-    public static CommentListDto from(Comment comment) {
-        return new CommentListDto(
-                comment.getId(),
-                comment.getNickname(),
-                comment.getContent(),
-                comment.isDeleted(),
-                comment.getCreatedAt(),
-                comment.getChildren().stream()
-                        .map(CommentListDto::from)
-                        .collect(Collectors.toList()));
-    }
-
     public static CommentListDto fromSingle(Comment comment) {
         return new CommentListDto(
                 comment.getId(),
+                comment.getAdminId(),
                 comment.getNickname(),
                 comment.getContent(),
                 comment.isDeleted(),
